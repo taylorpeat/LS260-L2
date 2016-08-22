44a103ed80430a3883c174fbdaec291b40e55999
Taylor Peat <taylorpeat@hotmail.com>
2016-08-21 22:04:42 -0400

    Update backbone project with backbone collection

--- multiple_models/javascripts/models.js
+++ multiple_models/javascripts/models.js
@@ -1,79 +1,85 @@
-var ItemModel,
-    items = [],
-    templates = {},
-    criteria = "name";
+var App = {
+  templates: {},
+  init: function() {
+    this.Items = new ItemCollection(items_json);
+    this.render();
+    this.bind();
+  },
+  bind: function() {
+    $("th").on("click", function(e) {
+      this.Items.comparator = $(e.target).attr("data-prop");
+      this.render();
+    }.bind(this) );
+
+    $("form").on("submit", function(e) {
+      e.preventDefault();
+
+      var itemProps = $(this).serializeArray(),
+          item = {};
+
+      itemProps.forEach(function(prop) {
+        item[prop.name] = prop.value;
+      });
+
+      App.Items.add(item);
+
+      App.render();
+    });
+
+    $("table").on("click", "td a", function(e) {
+      e.preventDefault();
+
+      var id = +$(e.target).attr("data-id");
+
+      App.Items.remove(id);
+
+      App.render();
+    });
+
+    $("p a").on("click", function(e) {
+      e.preventDefault();
+
+      this.Items.reset();
+      this.render();
+    }.bind(this) );
+  },
+  render: function() {
+    this.Items.sort();
+    $("tbody").html(this.templates.items({ items: this.Items.models }));
+  },
+};
 
 $("[type='text/x-handlebars']").each(function() {
   $template = $(this);
-  templates[$template.attr("id")] = Handlebars.compile($template.html());
+  App.templates[$template.attr("id")] = Handlebars.compile($template.html());
 });
 
 Handlebars.registerPartial("item", $("[id='item']").html());
 
-ItemModel = Backbone.Model.extend({
-  initialize: function() {
-    this.set("id", items.length + 1);
-    items.push(this);
-    render();
-  }
-});
+var ItemModel = Backbone.Model.extend({
+      initialize: function() {
+        this.collection.incrementId();
+        this.set("id", this.collection.last_id);
+      }
+    }),
+    ItemCollection = Backbone.Collection.extend({
+      initialize: function() {
+      },
+      model: ItemModel,
+      comparator: "name",
+      last_id: 0,
+      incrementId: function() {
+        this.last_id++
+      }
+    });
 
-items_json.forEach(function(item) {
-  new ItemModel(item);
-});
-
-render();
-
-$("[data-prop='name']").on("click", function() {
-  criteria = "name";
-  render();
-});
-
-$("th").on("click", function() {
-  criteria = $(this).attr("data-prop");
-  render();
-});
-
-$("form").on("submit", function(e) {
-  e.preventDefault();
-
-  var itemProps = $(this).serializeArray(),
-      item = new ItemModel();
-
-  itemProps.forEach(function(prop) {
-    item.set(prop.name, prop.value);
-  });
-
-  render();
-});
-
-$("table").on("click", "td a", function(e) {
-  e.preventDefault();
-
-  var id = +$(e.target).attr("data-id");
-
-  items = items.filter(function(item) {
-    return id !== item.toJSON().id;
-  });
-
-  render();
-});
-
-$("p a").on("click", function(e) {
-  e.preventDefault();
-
-  items = [];
-  render();
-});
+App.init();
 
 
-function render() {
-  sortItems();
-  $("tbody").html(templates.items({ items: items }));
-}
 
-function sortItems() {
-  items = _.sortBy(items, function(item) {
-    return item.toJSON()[criteria]
-  });
-}
+
+
+
+
+
+
