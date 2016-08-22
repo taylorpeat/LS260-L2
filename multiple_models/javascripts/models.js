788cda376cdc7a7407a931cb7d7911fe1f8011e0
Taylor Peat <taylorpeat@hotmail.com>
2016-08-22 15:37:53 -0400

    Add view to multiple models

--- multiple_models/javascripts/models.js
+++ multiple_models/javascripts/models.js
@@ -1,58 +1,34 @@
 var App = {
-  templates: {},
   init: function() {
     this.Items = new ItemCollection(items_json);
-    this.render();
+    this.View = new ItemsView({ collection: this.Items });
+    this.View.render();
     this.bind();
+  },
+  parseForm: function() {
+    return {
+       name: $("[name='name']").val(),
+       quantity: $("[name='quantity']").val()
+      };
   },
   bind: function() {
     $("th").on("click", function(e) {
       this.Items.comparator = $(e.target).attr("data-prop");
-      this.render();
+      this.Items.trigger("sortView");
     }.bind(this) );
-
+          
     $("form").on("submit", function(e) {
       e.preventDefault();
-
-      var itemProps = $(this).serializeArray(),
-          item = {};
-
-      itemProps.forEach(function(prop) {
-        item[prop.name] = prop.value;
-      });
-
-      App.Items.add(item);
-
-      App.render();
-    });
-
-    $("table").on("click", "td a", function(e) {
-      e.preventDefault();
-
-      var id = +$(e.target).attr("data-id");
-
-      App.Items.remove(id);
-
-      App.render();
-    });
+      App.Items.add(this.parseForm());
+    }.bind(this) );
 
     $("p a").on("click", function(e) {
       e.preventDefault();
-
       this.Items.reset();
-      this.render();
     }.bind(this) );
   },
-  render: function() {
-    this.Items.sort();
-    $("tbody").html(this.templates.items({ items: this.Items.models }));
-  },
-};
 
-$("[type='text/x-handlebars']").each(function() {
-  $template = $(this);
-  App.templates[$template.attr("id")] = Handlebars.compile($template.html());
-});
+};
 
 Handlebars.registerPartial("item", $("[id='item']").html());
 
@@ -63,13 +39,33 @@ var ItemModel = Backbone.Model.extend({
       }
     }),
     ItemCollection = Backbone.Collection.extend({
-      initialize: function() {
-      },
       model: ItemModel,
       comparator: "name",
       last_id: 0,
       incrementId: function() {
         this.last_id++
+      }
+    }),
+    ItemsView = Backbone.View.extend({
+      el: $("tbody"),
+      events: {
+        "click a": "removeItem"
+      },
+      template: Handlebars.compile($("#items").html()),
+      removeItem: function(e) {
+        e.preventDefault();
+
+        console.log("trying to remove");
+        var id = $(e.target).attr("data-id");
+
+        this.collection.remove(id);
+      },
+      initialize: function() {
+        this.listenTo(this.collection, "sortView add", this.collection.sort.bind(this.collection));
+        this.listenTo(this.collection, "remove reset add sortView", this.render);
+      },
+      render: function() {
+        this.$el.html(this.template({ items: this.collection.toJSON() }));
       }
     });
 
